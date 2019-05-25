1bfb1fe54981bac352bc1a9d35618053b51cf650
CengOne <aryandithree@gmail.com>
2019-05-25 19:43:27 +0700

    Update index.php

--- index.php
+++ index.php
@@ -27,8 +27,8 @@ $result = $blobClient->listBlobs($containerName, $listBlobsOptions);
  ?>
 
 <html>
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<!-- <script src="js/jquery-3.3.1.min.js"></script> -->
+<!-- <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script> -->
+<script src="js/jquery-3.3.1.min.js"></script>
 <!-- <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script> -->
 <script type="text/javascript">
 $(document).ready(function () {
