5369287494f407f9046b408f21659929dd637763
CengOne <aryandithree@gmail.com>
2019-05-25 19:58:35 +0700

    update

--- index.php
+++ index.php
@@ -365,23 +365,7 @@ $(document).ready(function () {
                 </div>
             </div>
 
-            <div class="row clearfix">
-                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                    <div class="card">
-                        <div class="body">
-                          <h4>
-                                Hasil Azure Computer Vision
-                            </h4>
-
-                          <div id="placeimage" style="width:420px; display:table-cell;">
-                    				<img id="imageid" width="400" />
-                    				<br>
-                    				<h3 id="detailsimages"></h3>
-                    			</div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <?php include 'loadhasil.php'; ?>
             <!-- #END# Example -->
         </div>
     </section>
---
+++ loadhasil.php
@@ -0,0 +1,17 @@
+<div class="row clearfix">
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+        <div class="card">
+            <div class="body">
+              <h4>
+                    Hasil Azure Computer Vision
+                </h4>
+
+              <div id="placeimage" style="width:420px; display:table-cell;">
+                <img id="imageid" width="400" />
+                <br>
+                <h3 id="detailsimages"></h3>
+              </div>
+            </div>
+        </div>
+    </div>
+</div>
